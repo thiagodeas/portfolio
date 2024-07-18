11d148ff908c848825c6da528ae766763f911762
thiagodeas <sousaalvesth@gmail.com>
2024-07-18 11:16:52 -0300

    adicionando tecnologias usadas nos projetos

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,21 +1,29 @@
-import { IoLogoGithub } from "react-icons/io"
+import { IoLogoGithub } from "react-icons/io";
 
 export interface CardProps {
   name: string;
   description: string;
   html_url: string;
+  languages?: string[];
 }
 
-export const Card = ({ name, description, html_url } : CardProps) => {
+export const Card = ({ name, description, html_url, languages }: CardProps) => {
   return (
-    <div 
-    className="bg-soft-blue w-[300px] h-[300px] rounded-[8px] flex flex-col items-center justify-between p-5 text-center shadow-md shadow-medium-blue"
-    >
-      <h3 className="font-semibold">{name}</h3>
-      <p>{description}</p>
-      <div className="flex items-center justify-center self-end">
-        <a href={html_url} target="_blank noreferrer noopener"><IoLogoGithub className="w-10 h-10 cursor-pointer rounded-[4px] hover:bg-primary-blue hover:p-[2px] transition-all duration-700" /></a>
+    <div className="flex flex-col items-center">
+      <div className="bg-soft-blue w-[300px] rounded-[8px] flex flex-col items-center justify-between p-5 text-center shadow-md shadow-light-blue">
+        <h3 className="font-semibold">{name}</h3>
+        <p>{description}</p>
+        <div className="flex items-center mt-4">
+          <a href={html_url} target="_blank" rel="noreferrer noopener" className="flex items-center gap-x-3 p-[8px] rounded-[6px] hover:bg-light-blue transition-all duration-700">
+            Acessar repositório: <IoLogoGithub className="w-8 h-8 cursor-pointer rounded-[4px]" />
+          </a>
+        </div>
       </div>
+      {languages && languages.length > 0 && (
+        <div className="bg-soft-blue mt-4  px-3 py-1 rounded-[4px] w-fit text-sm italic">
+          Linguagens: {languages.join(", ")}
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -1,37 +1,49 @@
-  "use client";
-  
-  import { Card, CardProps } from "./Card"
-  import { useEffect, useState } from "react";
-  import axios from "axios";
+"use client";
 
-  export const ProjectsSection = () => {
-    const [repositories, setRepositories] = useState<CardProps[]>([]);
+import { Card, CardProps } from "./Card";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-    useEffect(() => {
-      const fetchRepositories = async () => {
-        try {
-          const response = await axios.get('https://api.github.com/users/thiagodeas/repos');
-          setRepositories(response.data);
-        } catch (error) {
-          console.error("Houve um erro na requisição dos dados: ", error);
-        }
+export const ProjectsSection = () => {
+  const [repositories, setRepositories] = useState<CardProps[]>([]);
+
+  useEffect(() => {
+    const fetchRepositories = async () => {
+      try {
+        const response = await axios.get('https://api.github.com/users/thiagodeas/repos');
+        const repos = response.data;
+
+          const reposWithLanguages = await Promise.all(repos.map(async (repo) => {
+          const languagesResponse = await axios.get(`https://api.github.com/repos/thiagodeas/${repo.name}/languages`);
+          return {
+            ...repo,
+            languages: Object.keys(languagesResponse.data)
+          };
+        }));
+
+        setRepositories(reposWithLanguages);
+      } catch (error) {
+        console.error("Houve um erro na requisição dos dados: ", error);
       }
-      fetchRepositories();
-    }, []);
+    };
+    fetchRepositories();
+  }, []);
 
-    return (
-      <section id="projetos" className="w-full min-h-screen flex flex-col justify-center items-center pt-36 pb-20 px-4">
-        <h2 className="text-title font-semibold text-medium-blue tracking-[2px] text-center pb-12">Projetos</h2>
-        <div className="flex items-center justify-center gap-10 flex-wrap">
-          {repositories.map((r) => (
-            <Card
-              key={r.name}
-              name={r.name}
-              description={r.description}
-              html_url={r.html_url}
-            />
-          ))}
-        </div>
-      </section>
-    )
-  }
+  return (
+    <section id="projetos" className="w-full min-h-screen flex flex-col justify-center items-center pt-36 pb-20 px-4">
+      <h2 className="text-title font-semibold text-medium-blue tracking-[2px] text-center pb-12">Projetos</h2>
+      <div className="flex items-center justify-center gap-10 flex-wrap">
+        {repositories && repositories.length > 0 ? 
+        (repositories.map((r) => (
+          <Card
+            key={r.name}
+            name={r.name}
+            description={r.description}
+            html_url={r.html_url}
+            languages={r.languages}
+          />
+        ))) : (<p>Projetos em desenvolvimento...</p>)}
+      </div>
+    </section>
+  );
+};
