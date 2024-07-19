35242a7a9dec2c7e55d98374a92f60854d55642d
thiagodeas <sousaalvesth@gmail.com>
2024-07-19 16:11:49 -0300

    adicionando icones de redes sociais

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -1,14 +1,29 @@
-
 import { ContactForm } from "./ContactForm"
+import { FiGithub, FiInstagram, FiLinkedin } from "react-icons/fi";
 
 export const ContactSection = () => {
   return (
-    <section id="contato" className="custom-container">
+    <section id="contato" className="w-full min-h-screen flex justify-center items-center">
       <div>
         <h2 className="text-title font-bold">Contato</h2>
         <ContactForm />
       </div>
+        <div className="flex flex-col justify-center items-center gap-4">
+            <a href="https://www.linkedin.com/in/thiagodsousa/" target="_blank" rel="noopener noreferrer" className="flex items-center justify-center gap-x-2">
+              <FiLinkedin className="text-3xl" />
+              <span className="text-xl">/thiagodsousa</span>
+            </a>
+          <a href="https://github.com/thiagodeas" target="_blank" rel="noopener noreferrer" className="flex items-center justify-center gap-x-2">
+            <FiGithub className="text-3xl" />
+            <span className="text-xl">/thiagodeas</span>
+          </a>
+          <a href="https://www.instagram.com/thiagodeass/" target="_blank" rel="noopener noreferrer" className="flex items-center justify-center gap-x-2">
+            <FiInstagram className="text-3xl" />
+            <span className="text-xl">/thiagodeass</span>
+          </a>
+        </div>
 
+      
     </section>
   )
 }
