ae199001c13cb01ac3a6920261e7ddbd3ca3194f
thiagodeas <sousaalvesth@gmail.com>
2025-01-20 18:53:24 -0300

    atualizando dados

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -16,8 +16,8 @@ export const AboutSection = () => {
       </div>
       <div className="flex flex-col items-center justify-center w-[600px] text-center gap-y-5 max675:w-[550px] max620:w-[500px] max550:w-[450px] max480:w-[400px] max425:w-[350px] max425:gap-y-2 max375:w-[300px]">
         <h2 className="text-title font-semibold text-primary-blue tracking-[2px] max700:text-[2.5rem] max480:text-[2rem]">Sobre mim</h2>
-        <p className="text-[1.1rem] tracking-wide max700:text-[1rem] max700:tracking-[0px] max425:text-[15px] max400:text-[14px]">Meu nome é Thiago, tenho 23 anos e sou graduando em Análise e Desenvolvimento de Sistemas pela Estácio. Iniciei minha trajetória na programação com Python, aprofundando-me posteriormente em JavaScript, TypeScript e React, com mais de um ano de dedicação ao desenvolvimento Front-End.</p>
-        <p className="text-[1.1rem] tracking-wide max700:text-[1rem] max700:tracking-[0px] max425:text-[15px] max400:text-[14px]">Após essa experiência focada apenas em Front-End, decidi diversificar minhas habilidades e voltei meu foco para o Back-End. Atualmente, estou aprimorando meus conhecimentos em Java, buscando me tornar um profissional cada vez mais capacitado e completo no desenvolvimento de software.</p>
+        <p className="text-[1.1rem] tracking-wide max700:text-[1rem] max700:tracking-[0px] max425:text-[15px] max400:text-[14px]">Meu nome é Thiago, tenho 23 anos e sou um Desenvolvedor Full Stack fascinado por tecnologia. Iniciei minha trajetória na programação com Python, mas logo migrei para o universo JavaScript, onde dediquei mais de um ano de estudos em frameworks e ferramentas como React, Vue.js e Next.js.</p>
+        <p className="text-[1.1rem] tracking-wide max700:text-[1rem] max700:tracking-[0px] max425:text-[15px] max400:text-[14px]">Após essa experiência mais voltada para o Front End, entendi que estava na hora de ampliar minhas habilidades. Atualmente, estou aprimorando meus conhecimentos em Node.js, Laravel e Java.</p>
         <SocialMediaLinkAlt
         href="/assets/Thiago.pdf"
         icon={FaFileAlt}
