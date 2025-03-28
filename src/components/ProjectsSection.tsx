c8cf4fd2a62e64bde333a3a462ae6d5f1834abc4
thiagodeas <sousaalvesth@gmail.com>
2025-03-28 16:54:28 -0300

    adicionando projetos

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -5,12 +5,11 @@ import { ProjectCard } from "@/components/ProjectCard";
 export const ProjectsSection = () => {
   
   return (
-    <section id="projetos" className="w-full min-h-screen flex flex-col justify-center items-center pb-20 px-4" data-aos="zoom-in-up">
-      <h2 className="text-title font-semibold text-prim-text tracking-[2px] text-center p-14 max400:text-[2.5rem]">
+    <section id="projetos" className="w-full min-h-screen flex flex-col justify-center items-center pb-20 px-4" data-aos="fade-up">
+      <h2 className="text-title font-semibold text-prim-text tracking-[2px] text-center mt-12 p-14 max400:text-[2.5rem]">
         Projetos
       </h2>
-
-      <div className="flex items-center justify-center gap-x-24">
+      <div className="flex items-center justify-center gap-24 flex-wrap">
         <ProjectCard
             title="e-commerce"
             images={[
@@ -18,9 +17,9 @@ export const ProjectsSection = () => {
               { src: "/assets/ecommerce-p2.png", alt: "Page 2"},
               { src: "/assets/ecommerce-p3.png", alt: "Page 3"},
             ]}
-            description="Aplicação FullStack de um e-commerce."
+            description="Aplicação Full Stack de um e-commerce."
             frontend="Front End: Next.js, TypeScript e shadcn/ui."
-            backend="Back End: NestJS, MongoDB, Docker e Jest para testes unitários."
+            backend="Back End: NestJS, MongoDB, Docker, Jest para testes unitários e JWT para autenticação."
         />
 
         <ProjectCard
@@ -30,13 +29,34 @@ export const ProjectsSection = () => {
               { src: "/assets/cygni-p2.png", alt: "Page 2"},
               { src: "/assets/cygni-p3.png", alt: "Page 3"},
             ]}
-            description="Aplicação FullStack de um gerenciador de tarefas."
+            description="Aplicação Full Stack de um gerenciador de tarefas."
             frontend="Front End: React.js, TypeScript e Tailwind CSS."
-            backend="Back End: Java, Spring Boot, MySQL e Docker."
+            backend="Back End: Java, Spring Boot, MySQL, Docker e JWT para autenticação."
         />
+
+        <ProjectCard
+            title="curriculum-maker"
+            images={[
+              { src: "/assets/curriculum-maker-p1.png", alt: "Page 1"},
+              { src: "/assets/curriculum-maker-p2.png", alt: "Page 2"},
+              { src: "/assets/curriculum-maker-p3.png", alt: "Page 3"},
+            ]}
+            description="Aplicação Full Stack que gera um currículo minimalista em PDF através dos dados do usuário."
+            frontend="Front End: React, TypeScript e Styled Components."
+            backend="Back End: Java e Spring Boot."
+        />
+
+        <ProjectCard
+            title="login-page"
+            images={[
+              { src: "/assets/login-page-p1.png", alt: "Page 1"},
+              { src: "/assets/login-page-p2.png", alt: "Page 2"},
+            ]}
+            description="Aplicação FullStack de uma página de Login com autenticação."
+            frontend="Front End: TypeScript, Angular e SCSS."
+            backend="Back End: Java, Spring Boot, PostgreSQL e JWT para autenticação."
+        />  
       </div>
-      
-      
     </section>
   );
 };
