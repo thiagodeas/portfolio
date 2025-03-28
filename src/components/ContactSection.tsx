727c75ebff650ab13e665a4ec1646fcf36160c07
thiagodeas <sousaalvesth@gmail.com>
2025-03-28 11:37:14 -0300

    refatorando estilos do ContactSection

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -9,44 +9,44 @@ export const ContactSection = () => {
   return (
     <section id="contato" className="w-full min-h-screen flex justify-center items-center relative max1000:flex-col" data-aos="zoom-in-up">
       <div className="flex items-center justify-center flex-col">
-        <h2 className="text-center text-title font-bold text-primary-blue pb-3 max700:text-[2.5rem] max700:mt-8 max367:text-[2rem]">Entre em contato</h2>
-        <p className="text-center text-xl text-primary-blue opacity-65 pb-3 max575:text-[18px] max500:text-[1rem] max375:hidden">Sinta-se à vontade para entrar em contato comigo =)</p>
-        <div className="flex flex-col justify-center items-center gap-2 border-2 border-light-coral border-dashed rounded-lg p-4 mt-4">
+        <h2 className="text-center text-title font-bold text-prim-text pb-3 max700:text-[2.5rem] max700:mt-8 max367:text-[2rem]">Entre em contato</h2>
+        <p className="text-center text-xl text-details opacity-65 pb-3 max575:text-[18px] max500:text-[1rem] max375:hidden">Sinta-se à vontade para entrar em contato comigo =)</p>
+        <div className="flex flex-col justify-center items-center gap-2 border-2 border-sec-text border-dashed rounded-lg p-4 mt-4">
             <div className="flex items-center justify-center">
-            <span className="text-x1 pr-2 max550:hidden">No LinkedIn: </span>
-            <a href="https://www.linkedin.com/in/thiagodsousa/" target="_blank" rel="noopener noreferrer" className="flex items-center justify-center gap-x-2 rounded-lg p-1 hover:bg-primary-blue hover:text-light-coral transition-all ease-in-out duration-700">
+            <span className="text-x1 pr-2 text-details max550:hidden">No LinkedIn: </span>
+            <a href="https://www.linkedin.com/in/thiagodsousa/" target="_blank" rel="noopener noreferrer" className="flex items-center justify-center gap-x-2 rounded-lg p-1 text-details hover:text-prim-text transition-all ease-in-out duration-300">
               <FiLinkedin className="text-2xl" />
               <span className="text-xl">/thiagodsousa</span>
             </a>
             </div>
 
             <div className="flex items-center justify-center">
-             <span className="text-x1 pr-2 max550:hidden">No Instagram: </span>
-              <a href="https://www.instagram.com/thiagodeass/" target="_blank" rel="noopener noreferrer" className="flex items-center justify-center gap-x-2 rounded-lg p-1 hover:bg-primary-blue hover:text-light-coral transition-all ease-in-out duration-700">
+             <span className="text-x1 pr-2 text-details max550:hidden">No Instagram: </span>
+              <a href="https://www.instagram.com/thiagodeass/" target="_blank" rel="noopener noreferrer" className="flex items-center justify-center gap-x-2 rounded-lg p-1 text-details hover:text-prim-text transition-all ease-in-out duration-300">
               <FiInstagram className="text-2xl" />
               <span className="text-xl">/thiagodeass</span>
               </a>
             </div>
 
             <div className="flex items-center justify-center">
-            <span className="text-x1 pr-2 max550:hidden">Por Email: </span>
-            <a href="mailto:sousaalvesth@gmail.com" target="_blank" rel="noopener noreferrer" className="flex items-center justify-center gap-x-2 rounded-lg p-1 hover:bg-primary-blue hover:text-light-coral transition-all ease-in-out duration-700">
+            <span className="text-x1 pr-2 text-details max550:hidden">Por Email: </span>
+            <a href="mailto:sousaalvesth@gmail.com" target="_blank" rel="noopener noreferrer" className="flex items-center justify-center gap-x-2 rounded-lg p-1 text-details hover:text-prim-text transition-all ease-in-out duration-300">
             <CgMail className="text-2xl" />
             <span className="text-xl">sousaalvesth</span>
             </a>
             </div>
 
             <div className="flex items-center justify-center">
-            <span className="text-x1 pr-2 max550:hidden">Visite meu GitHub: </span>
-            <a href="https://github.com/thiagodeas" target="_blank" rel="noopener noreferrer" className="flex items-center justify-center gap-x-2 rounded-lg p-1 hover:bg-primary-blue hover:text-light-coral transition-all ease-in-out duration-700">
+            <span className="text-x1 pr-2 text-details max550:hidden">Visite meu GitHub: </span>
+            <a href="https://github.com/thiagodeas" target="_blank" rel="noopener noreferrer" className="flex items-center justify-center gap-x-2 rounded-lg p-1 text-details hover:text-prim-text transition-all ease-in-out duration-300">
             <FiGithub className="text-2xl" />
             <span className="text-xl">/thiagodeas</span>
           </a>
             </div>
             
-          <span className="max550:hidden">Ou, se preferir, me mande uma mensagem no WhatsApp: </span>
+          <span className="text-details max550:hidden">Ou, se preferir, me mande uma mensagem no WhatsApp: </span>
           <a href="https://api.whatsapp.com/send?phone=5585986988198&text=Oi,%20Thiago.%20Estou%20entrando%20em%20contato%20atrav%C3%A9s%20do%20seu%20site%20de%20portf%C3%B3lio." 
-          target="_blank" rel="noopener noreferrer" className="flex items-center justify-center gap-x-2 rounded-lg p-1 hover:bg-primary-blue hover:text-light-coral transition-all ease-in-out duration-700">
+          target="_blank" rel="noopener noreferrer" className="flex items-center justify-center gap-x-2 rounded-lg p-1 text-details hover:text-prim-text transition-all ease-in-out duration-300">
             <FaWhatsapp className="text-3xl" />
             <span className="text-xl">Vamos bater um papo!</span>
           </a>
