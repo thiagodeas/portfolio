11c7617ded0329dbd8d13a2f26eab3a0c1b095e4
thiagodeas <sousaalvesth@gmail.com>
2025-03-28 11:35:29 -0300

    refatorando estilos do IconContainer

--- src/components/IconContainer.tsx
+++ src/components/IconContainer.tsx
@@ -6,7 +6,7 @@ interface IconContainerProps {
 
 export const IconContainer = ({children}: IconContainerProps) => {
   return (
-    <div className="flex items-center justify-center flex-col text-center gap-2 shadow-md shadow-med-gray p-4 hover:shadow-lg hover:shadow-med-gray hover:scale-110 transition-all ease-in-out duration-500">
+    <div className="flex items-center justify-center flex-col text-center gap-2 p-4 hover:shadow-sm hover:scale-110 hover:shadow-sec-text transition-all ease-in-out duration-500">
       {children}
     </div>
   )
