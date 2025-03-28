47c51c49e0e8df124eb36372596effae4709256a
thiagodeas <sousaalvesth@gmail.com>
2025-03-28 11:33:24 -0300

    refatorando estilos da AboutSection

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -1,30 +1,22 @@
-import Image from "next/image"
-import { FaFileAlt } from "react-icons/fa"
-import { SocialMediaLinkAlt } from "./SocialMediaLinkAlt"
+import Image from "next/image";
 
 export const AboutSection = () => {
   return (
     <section id="sobre" className="flex w-full min-h-screen items-center justify-center gap-x-20 max1070:flex-col-reverse" data-aos="zoom-in-up">
       <div className="max1070:mt-0">
-        <Image
-        src="/assets/about-illustration.svg"
-        alt="about-illustration"
-        width={570}
-        height={500}
-        className="h-[500px] w-[450px] mr-6 max1070:hidden"
-         />
+      <Image
+          src="/assets/profile.jpeg"
+          alt="my-profile"
+          width={300}
+          height={300}
+          className="rounded-full border-primary-orange max1060:w-[350px] max1060:h-[350px] max700:w-[300px] max700:h-[300px] max675:hidden"
+        />
       </div>
       <div className="flex flex-col items-center justify-center w-[600px] text-center gap-y-5 max675:w-[550px] max620:w-[500px] max550:w-[450px] max480:w-[400px] max425:w-[350px] max425:gap-y-2 max375:w-[300px]">
-        <h2 className="text-title font-semibold text-primary-blue tracking-[2px] max700:text-[2.5rem] max480:text-[2rem]">Sobre mim</h2>
-        <p className="text-[1.1rem] tracking-wide max700:text-[1rem] max700:tracking-[0px] max425:text-[15px] max400:text-[14px]">Desenvolvedor Full Stack com experiência em Front-End (JavaScript, TypeScript, React, Vue) e Back-End (Node.js, Java, Docker, MongoDB). Atualmente, estagiário no setor de desenvolvimento da Fortics Tecnologia, colaborando na documentação, testes e requisitos.</p>
-        <p className="text-[1.1rem] tracking-wide max700:text-[1rem] max700:tracking-[0px] max425:text-[15px] max400:text-[14px]">Cursando o último semestre da graduação, busco constantemente aprimorar minhas habilidades, criando soluções escaláveis e eficientes.</p>
-        <SocialMediaLinkAlt
-        href="/assets/Thiago.pdf"
-        icon={FaFileAlt}
-        >
-        <span className="max550:text-[15px]">Download CV</span>                                                 
-        </SocialMediaLinkAlt>
+        <h2 className="text-title font-semibold text-prim-text tracking-[2px] max700:text-[2.5rem] max480:text-[2rem]">Sobre mim</h2>
+        <p className="text-[1.1rem] text-details tracking-wide max700:text-[1rem] max700:tracking-[0px] max425:text-[15px] max400:text-[14px]">Tenho 23 anos, sou desenvolvedor Full Stack autodidata apaixonado por tecnologia. Estou no último semestre em Análise e Desenvolvimento de Sistemas e gosto bastante de trabalhar em equipe, resolver problemas complexos e me aprimorar constantemente para entregar resultados cada vez melhores.</p>
+        <p className="text-[1.1rem] text-details tracking-wide max700:text-[1rem] max700:tracking-[0px] max425:text-[15px] max400:text-[14px]">Atualmente Estagiário de Desenvolvimento de Software na Fortics Tecnologia.</p>
       </div>
-    </                                                                                                    section>                                                                                                                  
+    </section>                                                                                                                  
   )
 }
