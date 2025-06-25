24cefcbd2c4a6bc71d76ddd2c36a1d845aff5191
thiagodeas <sousaalvesth@gmail.com>
2025-06-25 16:21:32 -0300

    chore(footer): update copyright year to 2025

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,9 @@
-  export const Footer = () => {
-    return (
-      <footer className="w-full h-16 bg-black flex items-center justify-center bottom-0 left-0 border-t border-dark-gray">
-        <p className="text-prim-text font-semibold text-sm tracking-wider text-center"> © 2024 | Thiago Sousa | Todos os direitos reservados</p>
-      </footer>
-    )
-  }
+export const Footer = () => {
+  return (
+    <footer className="w-full h-16 bg-black flex items-center justify-center bottom-0 left-0 border-t border-dark-gray">
+      <p className="text-prim-text font-semibold text-sm tracking-wider text-center">
+        © 2025 | Thiago Sousa | Todos os direitos reservados
+      </p>
+    </footer>
+  );
+};
